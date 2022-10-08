3d10cfb6898fafb97887d2c4ee9be8d813c3c19e
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-08 13:51:59 +0300

    Added routes and data

---
+++ db.js
@@ -0,0 +1,15 @@
+const animals = require("./data/animals.json");
+const vegetables = require("./data/vegetables.json");
+const fruits = require("./data/fruits.json");
+const fertilizers = require("./data/fertilizers.json");
+const users = require("./data/users.json");
+const comments = require("./data/comments.json");
+
+module.exports = {
+    animals,
+    vegetables,
+    fruits,
+    fertilizers,
+    users,
+    comments,
+};
